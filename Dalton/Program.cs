cced3e55534bc65a5802e4dee858f1f4b69282de
Carlos Kassab <carlos.kassab@nextiva.com>
2023-07-21 16:14:34 -0600

    Config dependency injection

--- Dalton/Program.cs
+++ Dalton/Program.cs
@@ -1,25 +1,16 @@
 using Dalton;
-using Dalton.Models;
 using Dalton.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Configuration;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-var apiSettings = new ApiSettings();
-builder.Configuration.GetSection("ApiSettings").Bind(apiSettings);
-builder.Services.AddSingleton(apiSettings);
-
 builder.Services.AddScoped<MyApiService>(); // Registro del servicio aqu�
-
 builder.Services.AddScoped(sp =>
 {
-    var apiUrl = "https://localhost:7202"; // Reemplaza con la URL base de tu API
-    return new HttpClient { BaseAddress = new Uri(apiUrl) };
+    return new HttpClient();
 });
 
 await builder.Build().RunAsync();
